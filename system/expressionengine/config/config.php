ac73970a47fb1814fa51daf7380ab03c33eb9caa
Ricardo Díaz <ricardotk002@gmail.com>
2014-06-10 16:29:14 -0500

    Update config.php

--- system/expressionengine/config/config.php
+++ system/expressionengine/config/config.php
@@ -24,8 +24,8 @@ $config['app_version'] = '273';
 $config['install_lock'] = "";
 $config['license_number'] = "5343-3237-2705-9088";
 $config['debug'] = "2";
-$config['cp_url'] = $admin_url;
-//$config['cp_url'] = "c:/xampp/htdocs/La-Positiva-Amazon/system/index.php";
+//$config['cp_url'] = $admin_url;
+$config['cp_url'] = "/var/www/packvehicular.com/system/index.php";
 $config['doc_url'] = "http://ellislab.com/expressionengine/user-guide/";
 $config['system_on'] = "y";
 $config['allow_extensions'] = 'y';
@@ -41,16 +41,16 @@ $config['site_url'] = $base_url;
 $config['server_path'] = FCPATH;
 $config['site_index'] = '';
 $config['theme_folder_url'] = $config['site_url']."/themes/";
-$config['theme_folder_path'] = $config['server_path']."themes\\";
+$config['theme_folder_path'] = $config['server_path']."/themes/";
 $config['save_tmpl_files'] = "y";
-//$config['tmpl_file_basepath'] = $config['server_path']."\\templates\\";
-$config['avatar_url'] = $base_url."\\uploads\\system\\avatars\\";
-$config['avatar_path'] = $config['server_path']."\\uploads\\system\\avatars\\";
-$config['photo_url'] = $base_url."\\uploads\\system\\member_photos\\";
-$config['photo_path'] = $config['server_path']."\\uploads\\system\\member_photos\\";
-$config['sig_img_url'] = $base_url."\\uploads\\system\\signature_attachments\\";
-$config['sig_img_path'] = $config['server_path']."\\uploads\\system\\signature_attachments\\";
-$config['prv_msg_upload_path'] = $config['server_path']."\\uploads\\system\\pm_attachments\\";
+//$config['tmpl_file_basepath'] = $config['server_path']."/templates/";
+$config['avatar_url'] = $base_url."/uploads/system/avatars/";
+$config['avatar_path'] = $config['server_path']."/uploads/system/avatars/";
+$config['photo_url'] = $base_url."/uploads/system/member_photos/";
+$config['photo_path'] = $config['server_path']."/uploads/system/member_photos/";
+$config['sig_img_url'] = $base_url."/uploads/system/signature_attachments/";
+$config['sig_img_path'] = $config['server_path']."/uploads/system/signature_attachments/";
+$config['prv_msg_upload_path'] = $config['server_path']."/uploads/system/pm_attachments/";
 
 /* CodeIgniter Configuration
 -------------------------------------------------------------------*/
